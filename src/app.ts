e4ef94df5e1aff5ca1506ac66b0c4d0b4a877c50
zaiatsFlagbit <zaiats85@mail.ru>
2021-05-04 18:03:28 +0200

    develop setup. typescript, nodemon, eslint, rxjs, dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,39 @@
+import dotenv from "dotenv";
 import express from "express";
+import path from "path";
+import { Observable } from "rxjs";
+
+// initialize configuration
+dotenv.config();
+
+// port is now available to the Node.js runtime
+// as if it were an environment variable
+const port = process.env.SERVER_PORT;
 
 const app = express();
-const port = 3000;
+
+const foo = new Observable((subscriber) => {
+  subscriber.next("Hello,");
+  subscriber.next("My name is...");
+  subscriber.next("Linus :)");
+  subscriber.complete();
+});
+
+foo.subscribe({
+  next: (x) => {
+    console.log(x);
+  },
+  // tslint:disable-next-line:object-literal-sort-keys
+  error: (err) => {
+    console.log("Error : " + err);
+  },
+  complete: () => {
+    console.log("Done...");
+  },
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello Dariya");
+  res.send("Hello Dariya and Stanislaw and Oleg, and Lyda");
 });
 
 // @ts-ignore
